160a433bcb77095fde8fa1c97d324bbf93df93b3
Chempearcedev <chempearce1@hotmail.com>
2021-08-01 14:35:13 +0000

    completed homepage header and css

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -1,7 +1,6 @@
 """boutique_ado URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,9 +14,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
