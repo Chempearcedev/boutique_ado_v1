4555452c6d1414026e8452d219d0dc72d15f5c53
Chempearcedev <chempearce1@hotmail.com>
2021-08-15 09:48:08 +0000

    added product format

--- products/models.py
+++ products/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Category(models.Model):
 
     class Meta:
-        verbose_name_plural = 'Categories'       
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
