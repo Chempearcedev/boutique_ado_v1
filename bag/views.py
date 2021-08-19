309e7bef67ecdf833a9c1dda52376c0b7349e8d7
Chempearcedev <chempearce1@hotmail.com>
2021-08-19 09:03:25 +0000

    finished product size logic

--- bag/views.py
+++ bag/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
-
 def view_bag(request):
     """ A view that renders the bag contents page """
 
@@ -13,12 +12,24 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
